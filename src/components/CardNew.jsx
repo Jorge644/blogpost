dfe77ecfa6a87d0eca50dd62e20c589e7156991f
Jorge Luis Castro <jcastro@technisys.com>
2021-05-29 20:27:54 -0300

    refactor

--- src/components/CardNew.jsx
+++ src/components/CardNew.jsx
@@ -13,7 +13,7 @@ const CardNew = () => {
 
             <div className="card-body">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus deleniti, iste culpa mollitia ipsum minus nesciunt, quam maiores illo laborum fuga repellat odio, voluptatem autem nemo alias est corrupti?</p>
-                <img 
+                <img
                     src="https://www.infobae.com/new-resizer/7SVIVlT083hbDOnV4natqyKcvXM=/992x661/filters:format(jpg):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/SK7XZVSKKBGBNILKE5FYN23YPQ.jpg" 
                     alt="foto principal"
                     width="100%"
