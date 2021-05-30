dde8a93cc09ac4dde58e39d5d7a2b1301cb701b8
Gabriel Berger <gab.berger@gmail.com>
2021-05-30 10:26:50 -0300

    Agrego logo en json y mejoro la UI de la carta de la noticia

--- src/components/CardNew.jsx
+++ src/components/CardNew.jsx
@@ -5,23 +5,33 @@ import React from "react"
 
 const CardNew = () => {
     return(
-        <div className="card" style={{width: "23rem"}}>
-            <div className="card-header">
-                <h3 className=" card-title text-warning">Lorem ipsum dolor sit amet consectetur adipisicing elit</h3>
-                <p>29 de Mayo 2021</p>
+        <div className="card rounded-3 m-2" style={{width:"25rem"}}>
+            <div>
+                <img 
+                    src="https://www.infobae.com/pf/resources/images/logo_infobae_naranja.svg?d=654" 
+                    alt="Icono Fuente Diario"
+                    className="img-fluid"
+                    style={{width:"4rem" , marginLeft:"0.5rem"}}
+                />
+                <hr className="mt-1 mb-0"/>  
             </div>
 
             <div className="card-body">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum delectus deleniti, iste culpa mollitia ipsum minus nesciunt, quam maiores illo laborum fuga repellat odio, voluptatem autem nemo alias est corrupti?</p>
-                <img
-                    src="https://www.infobae.com/new-resizer/7SVIVlT083hbDOnV4natqyKcvXM=/992x661/filters:format(jpg):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/SK7XZVSKKBGBNILKE5FYN23YPQ.jpg" 
-                    alt="foto principal"
-                    width="100%"
-                />
+               
+                <div className="position-relative">
+                    <img
+                        className="rounded-3"
+                        src="https://www.infobae.com/new-resizer/7SVIVlT083hbDOnV4natqyKcvXM=/992x661/filters:format(jpg):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/SK7XZVSKKBGBNILKE5FYN23YPQ.jpg" 
+                        alt="foto principal"
+                        width="100%"
+                    />
+                    <h4 className=" card-title text-warning position-absolute bottom-0 text-center fw-bold">Marcelo Gallardo, el mejor entrenador de los último 10 años</h4>
+                </div>
+              
             </div>
             
-            <div className="card-footer">
-                <p>Infobae - Sección Deportes</p>
+            <div className="mx-2">
+                <p>|Deportes| 29 de Mayo 2021</p>  
             </div>
         </div>
         
